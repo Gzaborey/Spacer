3187527bc8dc1d50783fb8a7ed3aaf36e5c5aa3c
George <gzaborey@gmail.com>
2024-03-20 17:54:53 +0200

    Minor changes

--- main.py
+++ main.py
@@ -12,16 +12,18 @@ if __name__ == '__main__':
     # Initialize BackgroundChanger object
     changer = Spacer(model)
 
-    image = cv2.imread('images/face.jpg')
-    background = cv2.imread('replacements/background.jpg')
+    cap = cv2.VideoCapture(0)
+    suc, image = cap.read()
+    # image = cv2.imread('images/face.jpg')
+    replacer = cv2.imread('replacements/image_space_1.jpg')
 
     image = cv2.resize(image, (160, 160))
-    background = cv2.resize(background, (160, 160))
+    replacer = cv2.resize(replacer, (160, 160))
 
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     # Starting the program
-    changed_image = changer(image, background).astype(np.uint8)
+    changed_image = changer(image, replacer).astype(np.uint8)
     changed_image = cv2.cvtColor(changed_image, cv2.COLOR_RGB2BGR)
     cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
     cv2.resizeWindow('Image', 512, 512)
